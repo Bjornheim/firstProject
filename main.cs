5d18863eeeb65a644497074c0d61d904f8a190a5
Ihor Tymoshenko <igorz.tymoszenko@gmail.com>
2024-12-04 19:40:11 +0200

    update main.cs

---
+++ main.cs
@@ -0,0 +1,64 @@
+// Логические типы данных:
+
+bool isChecked = true;
+bool isDead = false;
+bool isComplete = true;
+bool isEquip = false;
+bool isGrudge = true;
+
+// Числовые типы данных:
+
+int playerLevel = 28;
+int diceCount = 15;
+double currentDistance = 31.2;
+double goldCount = 2.986;
+float bodyTemperature = 36.9f;
+float currentDollarRate = 41.2f;
+decimal itemPrice = 325.5m;
+decimal targetDistance = 115.2m;
+byte steps = 108;
+byte currentLevel = 21;
+sbyte damageCount = 101;
+sbyte questCount = 151;
+short healthCount = 100;
+short playerDeathCount = 1001;
+ushort playerReputationRank = 65535;
+ushort goldLootedCount = 42500;
+uint enemyKillCount = 1000000;
+uint foodEatenCount = 502345;
+long chestLooted = 341;
+long dungeonCompleteCount = 13424;
+ulong questCompleted = 2716;
+ulong fishCaughtCount = 139;
+
+// Строчный тип данных:
+
+string greetingMessage = "Cousin Okri say's hello!";
+string questMessage = "This way adventurer, I have a key from treasure chest.";
+
+// Символьный тип данных:
+
+char grade = "F";
+char doorPassword = "x002f";
+
+// Тип перечислений:
+
+enum guildRanks 
+{
+    Master = 1,
+    Knights = 2,
+    Sergants = 3,
+    Adepts = 4
+}
+
+enum weaponList
+{
+    Sword,
+    Axe,
+    Hammer,
+    Spear,
+    Dagger,
+    Bow,
+    Crossbow,
+    Staff
+}
